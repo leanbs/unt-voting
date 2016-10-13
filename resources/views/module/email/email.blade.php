b91f91e69b258518a3efad47ca402d04c1aff786
leanbs <davin.yu1110@gmail.com>
2016-10-13 18:12:24 +0700

    Update email.blade.php

--- resources/views/module/email/email.blade.php
+++ resources/views/module/email/email.blade.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
- 	<meta charset="UTF-8">
- 	<title>Email</title>
-</head>
-<body>
-	Dear partisipan, terimakasih telah berpartisipasi dalam voting online ini! <br><br>
-	<br><br>
-	silahkan masukan kode berikut ini agar votingan yang anda berikan ke stan kesayangan anda menjadi valid
-	<h3>{{ $vote_code }}</h3>
-	<br><br><br><br><br>
-	~Terimakasih~,<br><br>
-	<p style="color:red;">Entrepreneurship Week 2016.</p>	
-</body>
-</html>
+
