6a7f645ef0a16d6e7e678ca1b7ca2b830ddf96f7
leanbs <davin.yu1110@gmail.com>
2016-10-16 17:44:18 +0700

    bersih2

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -32,7 +32,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="{{ asset('assets/js2/jquery.min.js') }}"></script>
     <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>   
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js2/bootstrap.min.js') }}"></script>
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
