3d29c7794489293268d2fe7a6b5e5904d661bd27
leanbs <davin.yu1110@gmail.com>
2016-10-23 18:43:02 +0700

    web friendly, secure_assets

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,14 +14,13 @@
     <title>Entrepreneurship Week 9th</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
-    <link href="{{ asset('assets/css/c3.min.css') }}" rel="stylesheet" type="text/css" >
-    <link href="{{ asset('assets/font-awesome-4.6.3/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
 
     <!-- Custom styles -->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css" >
-  </head>
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}" type="text/css" >
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/c3.min.css') }}">
 
   <body>
     @yield('page-content')
@@ -30,17 +29,17 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="{{ asset('assets/js2/jquery.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>   
-    <script src="{{ asset('assets/js2/bootstrap.min.js') }}"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
 
-    <script src="{{ asset('assets/js2/jquery-ui.min.js') }}"></script>
-    <script src="{{ asset('assets/js2/c3.min.js') }}"></script>
-    <script src="{{ asset('assets/js2/d3.min.js') }}"></script>
-    <script src="{{ asset('assets/js2/script.js') }}"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="{{ secure_asset('assets/js2/c3.min.js') }}"></script>  
+    <script src="{{ secure_asset('assets/js2/d3.min.js') }}"></script>  
+    <script src="{{ secure_asset('assets/js2/script.js') }}"></script>  
     <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
     <script src='https://www.google.com/recaptcha/api.js'></script>
